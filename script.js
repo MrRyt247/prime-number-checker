3174214ce2826c0f1081a338994007e8be1710c7
MrRyt247 <125740894+MrRyt247@users.noreply.github.com>
2023-07-08 17:32:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function check() {
+  const inp = document.getElementById("textbox");
+  const x = document.getElementById("textbox").value;
+  const result = document.getElementById("result");
+  const label = document.querySelector("label");
+  var isFilled = false;
+
+  prime = (a) => {
+    var divisor = 0;
+    var numOfDivisor;
+    for (let i = 1; i <= a / 2; i++) {
+      c = a / i;
+      if (c % 1 === 0) {
+        divisor += 1;
+        numOfDivisor = divisor + 1;
+      }
+    }
+
+    if (x == null || x <= 0) {
+      label.style.color = "red";
+      label.innerText = "Enter a counting number here:";
+      result.innerText = "";
+    } else if (numOfDivisor === undefined) {
+      label.style.color = "black";
+      result.innerText = `${x} is not a prime number.
+      It has 1 factor.`;
+    } else {
+      label.style.color = "black";
+      if (numOfDivisor === 2) {
+        result.innerText = `${x} is a prime number.
+        It has ${numOfDivisor} factors.`;
+      } else {
+        result.innerText = `${x} is not a prime number.
+        It has ${numOfDivisor} factors.`;
+      }
+    }
+  };
+  prime(x);
+}
+
+/*
+
+*/
