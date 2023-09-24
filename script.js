9a68476034029db2ebef2a68fbea754a8708ca66
MrRyt247 <125740894+MrRyt247@users.noreply.github.com>
2023-09-24 17:50:43 +0000

    oww

--- script.js
+++ script.js
@@ -24,8 +24,7 @@ function check() {
     if (x == null || x <= 0 || x - parseInt(x) !== 0) {
       /* label.style.color = "red"; */
       label.innerText = "Enter a counting number here:";
-      label.classList.remove("shake");
-      shake();
+        shake();
       result.innerText = "";
     } else if (numOfDivisor === undefined) {
       label.style.color = "black";
